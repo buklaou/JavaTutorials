8fb4551f066fa99a18746d443037c8ce47ddef3d
buklaou <nalbandianraffi@gmail.com>
2015-12-23 03:41:27 -0800

    Added Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,18 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
+        //prints Hello, World!
         System.out.println("Hello, World!");
+
+        //VARIABLES
+
+        int number = 5 + 5; //the number 10 is stored into the number variable
+        int secondNumber = 12; //the number 12 is stored into the secondNumber variable
+        int thirdNumber = 6; //the number 6 is stored into the thirdNumber variable
+
+        int myTotal = number + secondNumber + thirdNumber; //the myTotal variable adds all the numbers stored in the other variables
+
+
+        System.out.println(1000 - myTotal);
     }
 }
