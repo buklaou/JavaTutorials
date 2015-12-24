8a51ffa7bde3da57ddeef11ae7dd31aa0d6f3f1d
buklaou <nalbandianraffi@gmail.com>
2015-12-24 00:38:52 -0800

    More Data Types (+Some String)

--- src/DataTypesPt3.java
+++ src/DataTypesPt3.java
@@ -3,21 +3,25 @@
  */
 public class DataTypesPt3 {
 
-  /*  public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        *//*
-        width 16 (2 bytes)
-        char can only store one character #,A,$,^,% or a unicode character
-        for unicode characters go to: unicode-table.com/en/#control-character
-        char myChar = '\u00A5'; //need to put \u before unicode character 00A9
-        System.out.println(myChar);
-        *//*
+      /* width 16 (2 bytes)
+       char can only store one character #,A,$,^,% or a unicode character
+       for unicode characters go to: unicode-table.com/en/#control-character */
+       char myChar = '\u00A5'; //need to put slash and then u before unicode character 00A9
+       System.out.println(myChar);
 
-        boolean myBoolean = true;
-        boolean isMale = true;
 
-        *//*char registeredSymbol = '\u00AE';
-        System.out.println(registeredSymbol);*//*
+       boolean myBoolean = true;
+       boolean isMale = true;
 
-    }*/
+       /* Challenge
+       Find code for the registered symbol on the same line as the copyright symbol and then
+       create a variable of type char and assign it the unicode value for that symbol and print.
+        */
+       char registeredSymbol = '\u00AE';
+       System.out.println(registeredSymbol);
+
+
+   }
 }
--- src/DataTypesPt4.java
+++ src/DataTypesPt4.java
@@ -10,9 +10,21 @@ public class DataTypesPt4 {
 
 
         String myString = "This is a string";
-        System.out.println(myString);
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + "\u00A9 2015";
+        System.out.println("myString is equal to " + myString);
 
+        //Treated as text but java merges it into 250.5549.95 because they are both String
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("The result is " + numberString);
 
-
+        //Treated as text but java merges 1050 because it recognized the int value
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
     }
 }
