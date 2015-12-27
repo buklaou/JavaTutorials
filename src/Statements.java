1c5c047386a761dee59d2124ff531577a488248d
buklaou <nalbandianraffi@gmail.com>
2015-12-26 16:42:58 -0800

    Statements

---
+++ src/Statements.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Raffi on 12/26/2015.
+ */
+public class Statements {
+
+    public static void main(String[] args) {
+
+        //Keywords in java are int, double, System.out.println, public class, package, etc
+        //The expression portion of the code int myVariable = 50; is "myVariable = 50"
+
+        //These are all statements. They usually end with a semicolon.
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        //Java continues to search for the semicolon that would end this print statement
+        //even if it is not on the same line
+        System.out.println("This is" +
+                " another" +
+                " still more");
+
+
+        //this is whitespace which java will ignore
+        int                  whitespace
+                  =
+                  20;
+
+
+
+
+
+    }
+
+
+
+
+
+}
