b1259cb40e938c052735ad352c2238a615f8c835
buklaou <nalbandianraffi@gmail.com>
2016-01-17 16:42:04 -0800

    Autoboxing and Unboxing Part 2

---
+++ src/AutoboxingAndUnboxingPt2/Branch.java
@@ -0,0 +1,55 @@
+package AutoboxingAndUnboxingPt2;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Raffi on 1/17/2016.
+ */
+public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String customerName, double initialAmount) {
+        if(findCustomer(customerName) == null) {
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+        if(existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+
+        return false;
+    }
+
+    private Customer findCustomer(String customerName) {
+        for(int i = 0; i < customers.size(); i++) {
+            Customer checkedCustomer = this.customers.get(i);
+            if(checkedCustomer.getName().equals(customerName)) {
+                return checkedCustomer;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/AutoboxingAndUnboxingPt2/Customer.java
@@ -0,0 +1,30 @@
+package AutoboxingAndUnboxingPt2;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Raffi on 1/17/2016.
+ */
+public class Customer {
+
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double initialAmount) {
+        this.name = name;
+        this.transactions = new ArrayList<>();
+        addTransaction(initialAmount);
+    }
+
+    public void addTransaction(double amount) {
+        this.transactions.add(amount);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+}
